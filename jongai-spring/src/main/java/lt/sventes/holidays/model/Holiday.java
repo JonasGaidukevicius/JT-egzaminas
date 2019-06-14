75d499fa91ea0783ae911eedd4c58aff058af86b
Jonas <jgaidukevicius@gmail.com>
2019-06-14 20:07:25 +0300

    Nenaudosiu šios šakos

--- jongai-spring/src/main/java/lt/sventes/holidays/model/Holiday.java
+++ jongai-spring/src/main/java/lt/sventes/holidays/model/Holiday.java
@@ -2,18 +2,23 @@ package lt.sventes.holidays.model;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
 import lt.sventes.countries.model.Country;
 
 @Entity
+@Table(name = "holiday", indexes = { @Index(name = "idx_code", columnList = "code", unique = true),
+		@Index(name = "idx_title", columnList = "title", unique = true) })
 public class Holiday {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
